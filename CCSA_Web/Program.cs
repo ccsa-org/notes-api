6737b1fac6a4c37e29eb129cf9eb9e2e4a7636c3
Trinity Sunday <45539206+codequoia@users.noreply.github.com>
2022-08-19 09:48:11 +0100

    Removed Swagger usage restriction

--- CCSA_Web/Program.cs
+++ CCSA_Web/Program.cs
@@ -23,11 +23,11 @@ builder.Services.AddSingleton<SessionFactory>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
